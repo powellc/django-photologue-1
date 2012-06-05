3e0e9b84583a899ded4767b2f40e691a1c2db01a
Richard Barran <richard@arbee-design.co.uk>
2012-06-05 13:52:49 +0100

    Imports for unit test files were broken. Same bug as http://code.google.com/p/django-photologue/issues/detail?id=23.

--- photologue/tests/effect.py
+++ photologue/tests/effect.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-import Image
+try:
+    import Image
+except ImportError:
+    # Required PIL classes may or may not be available from the root namespace
+    # depending on the installation method used.
+    try:
+        from PIL import Image
+    except ImportError:
+        raise ImportError('Photologue was unable to import the Python Imaging\
+         Library. Please confirm it`s installed and available on your current\
+          Python path.')
+
 from photologue.models import PhotoEffect
 from photologue.tests.helpers import PhotologueBaseTest
 
--- photologue/tests/photo.py
+++ photologue/tests/photo.py
@@ -1,4 +1,14 @@
-import Image
+try:
+    import Image
+except ImportError:
+    # Required PIL classes may or may not be available from the root namespace
+    # depending on the installation method used.
+    try:
+        from PIL import Image
+    except ImportError:
+        raise ImportError('Photologue was unable to import the Python Imaging\
+         Library. Please confirm it`s installed and available on your current\
+          Python path.')
 import os
 from django.conf import settings
 from django.core.files.base import ContentFile
